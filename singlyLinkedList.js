f4af7eb1367d5df3c55113bd6b052bb3f2e939e1
Le Lin <lelin999@gmail.com>
2017-06-29 13:12:01 -0700

    implementaion of removeDuplicate complete

---
+++ singlyLinkedList.js
@@ -0,0 +1,61 @@
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+		this.count = 0;
+	}
+
+	length() {
+		return this.count;
+	}
+
+	display() {
+		return this;
+	}
+
+	add(val) {
+		const node = {
+			val,
+			next: null
+		}
+		if (!this.head) {
+			this.head = node;
+			this.count++;
+			return;
+		}
+		let current = this.head;
+		while (current.next) {
+			current = current.next;
+		}
+		current.next = node;
+		this.count++;
+	}
+
+	removeDuplicates() {
+		let hash = {};
+		let current = this.head;
+		let runner = this.head.next;
+		hash[current.val] = true;
+		while (runner) {
+			if (!(hash[runner.val])) {
+				hash[runner.val] = true;
+			} else {
+				if (runner.next) {
+					current.next = current.next.next;	
+				} else {
+					current.next = null;
+				}
+				this.count--;
+			}
+			current = current.next;
+			runner = runner.next;
+		}
+		return this;
+	}
+
+};
+
+let list1 = new SinglyLinkedList();
+list1.add(3);
+list1.add(5);
+list1.add(4);
+list1.add(3);
